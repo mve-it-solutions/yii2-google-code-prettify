c1d4adc78567c0e2a525b851e59d85db6c833f59
MvE-IT-Solutions <development@mvedesigns.eu>
2015-11-28 20:22:28 +0100

    initial commit

---
+++ GoogleCodePrettifyAsset.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace mveitsolutions\googlecodeprettify;
+
+use yii\web\AssetBundle;
+
+class GoogleCodePrettifyAsset extends AssetBundle 
+{
+    public static $options = [];
+    
+    public $sourcePath = '@vendor/mveitsolutions/yii2-google-code-prettify/assets';
+    public $js = ['js/prettify.js'];
+    public $css = ['css/prettify.css'];
+    public $depends = ['yii\web\JqueryAsset'];
+
+    /**
+     * set style css file of assetbundle 
+     */
+    public function init()
+    {
+		if (is_array(self::$options)) {
+			$this->css = ['css/'.self::$options['style'].'.css'];
+		}
+        parent::init();
+    }
+
+    /**
+     * @param \yii\web\View $view
+     * @param string $sStyle
+     * @return static
+     */
+    public static function register($oView, $aOptions=[])
+    {
+		$js = "";
+		if (is_array($aOptions)) {
+			self::$options['style'] = (isset($aOptions['style'])) ? $aOptions['style'] : 'prettify';
+			self::$options['lineNums'] = (isset($aOptions['lineNums'])) ? $aOptions['lineNums'] : false;
+			self::$options['autoAddPreClasses'] = (isset($aOptions['autoAddPreClasses'])) ? $aOptions['autoAddPreClasses'] : false;
+
+			if (self::$options['lineNums'] === true) {
+				$oView->registerCss('li.L0, li.L1, li.L2, li.L3, li.L5, li.L6, li.L7, li.L8 { list-style-type: decimal !important }');
+			}
+			if (self::$options['autoAddPreClasses'] === true) {
+				$js .= "
+					$('pre').each(function () {
+					    var langClass = $(this).attr('class') || '';
+					    $('pre').addClass('prettyprint ". (self::$options['lineNums'] ? linenums : '') ."' + langClass);
+					});
+				";
+			}
+			$js .= "
+				prettyPrint();
+			";
+		}
+
+        $oView->registerJs($js);
+
+        return parent::register($oView);
+    }
+}
---
+++ README.md
@@ -0,0 +1,41 @@
+# Yii2 Google Code Prettify
+
+Yii2 Code syntax highlighter using **[Google Code Prettify](https://github.com/google/code-prettify)**
+
+## Installation
+
+### Composer
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require --prefer-dist mve-it-solutions/yii2-google-code-prettify "*"
+```
+
+or add
+
+```
+"mve-it-solutions/yii2-google-code-prettify": "*"
+```
+
+to the require section of your `composer.json` file.
+
+## Usage
+
+To use this extension add below to your main layout:
+
+```php
+\mveitsolutions\googlecodeprettify\GoogleCodePrettifyAsset::register($this, ['style' => 'simple', 'lineNums' => true, 'autoAddPreClasses' => true]);
+```
+## AssetBundle Register Options
+	
+	**style**	Google Code Prettify Skin (Choose: desert, doxy, prettify, sons-of-obsidian, sunburst or simple)
+	**lineNums**	Put line numbers on every line (Choose: true or false)
+	**autoAddPreClasses**	Automatically add classes to pre (Choose: true or false)
+
+## License
+
+yii2-google-code-prettify is released under the MIT License. See [LICENSE.md](https://github.com/mve-it-solutions/yii2-google-code-prettify/blob/master/LICENSE.md) file for
+details.
